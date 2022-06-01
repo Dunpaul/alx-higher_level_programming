0ab346b5618f2f8bf3b514394e71f542da4c1e2a
Dunpaul <mwasmaish86@gmail.com>
2022-06-01 05:11:35 -0700

    fizzbuz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    for fizbuz in range(1,100):
+        if fizbuz % 15 == 0:
+            print("FizzBuzz ", end='')
+        elif fizbuz % 3 == 0:
+            print("Fizz ", end='')
+        elif fizbuz % 5 == 0:
+            print("Buzz ", end='')
+        else:
+            print("{} ".format(fizbuz), end='')
