1720f9cd09098d46bff3f977fd4789592bcc9a63
Dunpaul <mwasmaish86@gmail.com>
2022-06-07 05:11:19 -0700

    ficx

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    str_ls = list(my_string)
-    for chr in str_ls:
-        if chr == 'c' or chr == 'C':
-            str_ls.remove(chr)
-    newSt = ''.join(str_ls)
-    return(newSt)
+    new_str = ""
+    for i in my_string:
+        if ord(i) != 99 and ord(i) != 67:
+            new_str += i
+    return (new_str)
